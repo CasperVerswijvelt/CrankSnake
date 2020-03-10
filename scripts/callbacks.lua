9f28cf23f6110bd2eea48f021b817fe457ed18ea
CasperVerswijvelt <casperverswijvelt@gmail.com>
2020-03-10 21:14:56 +0100

    Comment out global unused function

--- scripts/callbacks.lua
+++ scripts/callbacks.lua
@@ -16,18 +16,18 @@ local food = nil
 
 local startScreenActive = true
 
-function tprint (tbl, indent)
-  if not indent then indent = 0 end
-  for k, v in pairs(tbl) do
-    formatting = string.rep("  ", indent) .. k .. ": "
-    if type(v) == "table" then
-      print(formatting)
-      tprint(tostring(v), indent+1)
-    else
-      print(formatting .. tostring(v))
-    end
-  end
-end
+--function tprint (tbl, indent)
+--  if not indent then indent = 0 end
+--  for k, v in pairs(tbl) do
+--    formatting = string.rep("  ", indent) .. k .. ": "
+--    if type(v) == "table" then
+--      print(formatting)
+--      tprint(tostring(v), indent+1)
+--    else
+--      print(formatting .. tostring(v))
+--    end
+--  end
+--end
 
 --[[
 
